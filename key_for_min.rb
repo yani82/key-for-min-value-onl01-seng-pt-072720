6f0b72f1244828d8e8233594044fd34cdb29f962
Yani Asari <yani.iman1@gmail.com>
2020-08-06 19:54:37 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
   
 low_value = 0 
 low_key = nil 
-name_hash.each do [key, value]
+name_hash.each do |key, value|
 if low_value == 0 || value < low_value
   low_value = value 
   low_key = key
